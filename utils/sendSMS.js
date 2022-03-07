5480fb0f9610b1035a49b2c503cf31cdf957c4dc
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-03-08 02:53:18 +0900

    fix: Separating the text sending logic into a util function

---
+++ utils/sendSMS.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+const { date, signature } = require("./smsHeader");
+const { URL } = require("../constant");
+
+const sendSMS = async ({ info }) => {
+  const { message, phoneNumber } = info;
+  const { NCP_ACCESS_KEY, CALLER_ID } = process.env;
+
+  await axios({
+    method: "POST",
+    json: true,
+    url: URL,
+    headers: {
+      "Content-Type": "application/json",
+      "x-ncp-iam-access-key": NCP_ACCESS_KEY,
+      "x-ncp-apigw-timestamp": date,
+      "x-ncp-apigw-signature-v2": signature,
+    },
+    data: {
+      type: "SMS",
+      contentType: "COMM",
+      countryCode: "82",
+      from: CALLER_ID,
+      content: message,
+      messages: [
+        {
+          to: `${phoneNumber}`,
+        },
+      ],
+    },
+  });
+};
+
+module.exports = sendSMS;
